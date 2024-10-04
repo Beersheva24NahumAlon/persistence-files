be3ef3e1f8ac052830966380aec36bc996bd01df
Nahum Alon <nochum.shepsl@gmail.com>
2024-10-04 16:00:19 +0300

    Chenged approch in method printDirectoryContent (used method walkFileTree)

--- src/test/java/telran/persistence/StringStreamsTest.java
+++ src/test/java/telran/persistence/StringStreamsTest.java
@@ -4,15 +4,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.*;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class StringStreamsTest {
     private static final String PRINT_STREAM_FILE = "printStreamFile.txt";
     private static final String PRINT_WRITER_FILE = "printWriterFile.txt";
+    private static final String SHIFT = "   ";
+
+    public class MyFileVisitor extends SimpleFileVisitor<Path> {
+        Path root;
+
+        public MyFileVisitor(Path root) {
+            this.root = root;
+        }
+
+        private void dysplayPath(Path path, String msg) {
+            int count = path.getNameCount() - root.getNameCount();
+            System.out.printf("%s%s%s\n", SHIFT.repeat(count), path.getFileName(), msg);
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            dysplayPath(dir, "");
+            return FileVisitResult.CONTINUE;
+        }
+    
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            dysplayPath(file, "");
+            return FileVisitResult.CONTINUE;
+        }
+    
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) { 
+            dysplayPath(file, " - can't access to path (input/output error)");     
+            return FileVisitResult.CONTINUE;
+        }
+    }
 
     @Test
+    @Disabled
     void printStreamTest() throws FileNotFoundException {
         PrintStream printStream = new PrintStream(PRINT_STREAM_FILE);
         printStream.println("Hello");
@@ -28,6 +62,7 @@ public class StringStreamsTest {
     }
 
     @Test
+    @Disabled
     void bufferedReaderTest() throws Exception {
         BufferedReader reader = new BufferedReader(new FileReader(PRINT_WRITER_FILE));
         assertEquals("Hello", reader.readLine());
@@ -35,26 +70,12 @@ public class StringStreamsTest {
     }
 
     @Test 
-    void printingDirectoryTest() throws Exception {
-        printDirectoryContent("C:\\Users\\Admin\\Documents", 3);
+    void printingDirectoryTest() throws IOException {
+        printDirectoryContent("C:\\Windows\\appcompat", 4); 
     }
 
-    private void printDirectoryContent(String pathString, int depth) throws IOException {
+    private void printDirectoryContent(String pathString, int depth) throws IOException{
         Path path = Path.of(pathString);
-        try {
-            Files.walk(path, depth).forEach(p -> printPath(p, path));
-        } catch (Exception e) {
-            // TODO: handle exception
-        } 
-    }
-
-    private int levelOfFileSystem(Path path) {
-        return path.toAbsolutePath().toString().split("\\\\").length;
-    }
- 
-    private void printPath(Path currentPath, Path rootPath) {
-        String shift = "   ";
-        int countOfShifts = levelOfFileSystem(currentPath) - levelOfFileSystem(rootPath);
-        System.out.printf("%s%s\n",shift.repeat(countOfShifts), currentPath.getFileName());
-    }
+        Files.walkFileTree(path, EnumSet.of(FileVisitOption.FOLLOW_LINKS), depth, new MyFileVisitor(path));
+    } 
 }
